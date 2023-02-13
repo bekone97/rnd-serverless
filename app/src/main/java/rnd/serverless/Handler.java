d0a9d6e7a6cd649b89079d7c67bc12c8480e8d33
a.lisouski <a.lisouski@godeltech.com>
2023-02-13 19:51:59 +0100

    refactoring

--- app/src/main/java/rnd/serverless/Handler.java
+++ app/src/main/java/rnd/serverless/Handler.java
@@ -2,22 +2,22 @@ package rnd.serverless;
 
 import rnd.serverless.api.Calculate;
 
-import java.util.Optional;
 import java.util.ServiceLoader;
 import java.util.stream.Collectors;
 
 public class Handler {
-
-    private final static String IMPL = Optional.ofNullable(System.getenv("IMPL")).orElse("NULL");
+    private final static String IMPL = "IMPL";
+    private final static String NULL = "NULL";
 
     public static void main(String[] args) {
         System.out.println(new Handler().handleRequest());
     }
 
     public String handleRequest() {
+        String impl = System.getenv().getOrDefault(IMPL, NULL);
         ServiceLoader<Calculate> services = ServiceLoader.load(Calculate.class);
         return services.stream()
-                .filter(calculateProvider -> IMPL.equals(calculateProvider.type().getName()))
+                .filter(calculateProvider -> impl.equals(calculateProvider.type().getName()))
                 .map(service -> service.get().calculate())
                 .collect(Collectors.joining("\n----------\n"));
     }
